fd824456e3e0ead52b9bb5484b9d9b14ec1d3c22
khoahoang9999-boop <khoahoang9999@gmail.com>
2026-05-16 02:37:37 +0700

    feat: Add zip utility for build packaging
    Introduces a new `zip.js` script to create a zip archive of the `extension-build` directory. This is intended for packaging the application for distribution or deployment.

---
+++ zip.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import archiver from 'archiver';
+
+const output = fs.createWriteStream('./extension-build.zip');
+const archive = archiver('zip', { zlib: { level: 9 } });
+
+output.on('close', function() {
+  console.log(archive.pointer() + ' total bytes zip created.');
+});
+
+archive.on('error', function(err) {
+  throw err;
+});
+
+archive.pipe(output);
+archive.directory('extension-build/', false);
+archive.finalize();
